a88765b3078d01902c00fd1fbb0fae504476cae1
Claudio Savelli <claudiosav2000@gmail.com>
2022-03-28 16:41:22 +0100

    3.1, 3.2, 3.3
    Finished first 3 points of tp3

--- tp3/MyUnitCube.js
+++ tp3/MyUnitCube.js
@@ -6,7 +6,7 @@ import {CGFobject} from '../lib/CGF.js';
  */
 export class MyUnitCube extends CGFobject {
 	constructor(scene) {
-		super(scene);
+		super(scene); 
 		this.initBuffers();
 	}
 	
@@ -42,6 +42,17 @@ export class MyUnitCube extends CGFobject {
 			0, 3, 5,
 			0, 5, 2
 
+		];
+
+		this.normals = [
+			-0.5, -0.5, -0.5,	//0
+			0.5, -0.5, -0.5,	//1
+			-0.5, 0.5, -0.5,	//2
+			-0.5, -0.5, 0.5,	//3
+			0.5, 0.5, -0.5,		//4
+			-0.5, 0.5, 0.5,		//5
+			0.5, -0.5, 0.5,		//6
+			0.5, 0.5, 0.5,		//7
 		];
 
 		//The defined indices (and corresponding vertices)
